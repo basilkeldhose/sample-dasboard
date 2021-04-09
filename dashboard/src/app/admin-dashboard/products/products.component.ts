0cb0dbe167c283b0580a26e1fb0ab66b9f28672b
Basil K Eldhose <beldhose2015@gmail.com>
2021-04-09 15:36:57 +0530

    complete frontent design anf responsive issues

--- dashboard/src/app/admin-dashboard/products/products.component.ts
+++ dashboard/src/app/admin-dashboard/products/products.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+productList=[{name:"sample1",
+  originalPrice:300,
+  totalPrice:140,
+  Dryclean:10,
+  leadprice:120,
+  ownerprice:0,
+  brand:"puma",
+  category:"top",
+  material:"cotton",
+  care:"dry clean only",
+  color:"black",
+  size:30
+}]
+show:boolean =false
+panelExpand:boolean=false
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  Toggle(i:number){
+    this.show =!this.show;
+  }
+  open(i:number){
+    this.panelExpand=!this.panelExpand
+  }
+  delete(){
+
+  }
+  update(){
+
+  }
 }
